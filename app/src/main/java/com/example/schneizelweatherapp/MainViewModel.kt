4823ab49db49a34af7738ff43d63d0665ce52c57
Александр Абхазава <sashkallg@yandex.ru>
2022-11-28 17:01:06 +0300

    123

---
+++ app/src/main/java/com/example/schneizelweatherapp/MainViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.schneizelweatherapp
+
+import android.annotation.SuppressLint
+import androidx.lifecycle.ViewModel
+import com.example.schneizelweatherapp.service.WeatherRepository
+import com.example.schneizelweatherapp.service.dto.CurrentWeather
+import com.example.schneizelweatherapp.service.dto.FullWeather
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@SuppressLint("MissingPermission")
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    repository: WeatherRepository
+) : ViewModel() {
+
+    val current: Flow<CurrentWeather> = repository.getCurrentWeather()
+    val forecast: Flow<List<FullWeather.Daily>> = repository.getFiveDayForecast()
+
+}
---
+++ app/src/main/java/com/example/schneizelweatherapp/ui/theme/Theme.kt
@@ -0,0 +1,25 @@
+package com.example.schneizelweatherapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Purple200,
+    primaryVariant = Purple700,
+    secondary = Teal200
+)
+
+@Composable
+fun SchneizelWeatherAppTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
